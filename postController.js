0a42cefc009284b1668b07dc038ff3fc94accbd1
mouhamed2213 <Mouhamedbk1002@gmail.com>
2025-10-13 23:11:47 +0000

    test

---
+++ postController.js
@@ -0,0 +1,13 @@
+// post object
+
+const post = [
+  {
+    id: 1,
+    title: "hello my first post",
+    desciption: "Post Description",
+  },
+];
+
+const getPost = () => post;
+
+export default getPost;
